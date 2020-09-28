16f4062a523be3315f5b2ef4283181bcaadcffc6
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2020-09-28 18:58:10 +0200

    add scanner function

--- hangman/input.go
+++ hangman/input.go
@@ -1,10 +1,28 @@
 package hangman
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
 
 func ReadGuess() (guess string, err error) {
 	valid := false
 	for valid != true {
 		fmt.Print("Choose a letter")
+		guess, err := reader.ReadString('\n')
+		if err != nil {
+			return guess, err
+		}
+		guess = strings.TrimSpace(guess)
+		if len(guess) != 1 {
+			fmt.Print("You just need to insert one char")
+			continue
+		}
+		valid = true
 	}
+	return guess, nil
 }
--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"hangman/hangman"
+	"os"
 )
 
 func main() {
 	g := hangman.New(8, "golang")
 	fmt.Println(g)
+
+	l, err := hangman.ReadGuess()
+	if err != nil {
+		fmt.Printf("Cannot read the input")
+		os.Exit(1)
+	}
+	fmt.Print(l)
 }
